c72610580662c2da4a485a87d83ca6a368543c7a
kbasye <kbasye@gmail.com>
2013-05-06 08:42:54 -0700

    Find the largest palindrome made from the product of two 3-digit numbers

---
+++ 01_multiples_of_3_and_5/01_01.rb
@@ -0,0 +1,18 @@
+module	MultipleSummer
+	extend self
+	def multiple_of?(dividend, divisor)
+		dividend % divisor == 0
+	end
+	def select_multiples(from, divisors=[])
+		raise 'must select multiples from a number greater than 0' if from <= 0
+		divisors.map { |divisor| 1.upto(from-1).to_a.select { |dividend| multiple_of?(dividend, divisor) } }.flatten!.uniq
+	end
+	def self.sum_multiples(from, divisors=[])
+		select_multiples(from, divisors).inject(:+)
+	end
+end
+
+MultipleSummer.sum_multiples(1000, [3,5]) == 233168
+
+# raises error if number is less than or equal to 0
+# MultipleSummer.sum_multiples(-1, [3,5])
